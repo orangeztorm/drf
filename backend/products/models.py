29c1e9b10442ad90a30b7307ea76ae7e2187b077
Taiwo Kenny <taiwokenny45@gmail.com>
2022-10-23 11:36:17 +0100

    django model data as api response

---
+++ backend/products/models.py
@@ -0,0 +1,12 @@
+from decimal import Decimal
+from email.policy import default
+from django.db import models
+
+# Create your models here.
+
+
+class Product(models.Model):
+    title = models.CharField(max_length=120)
+    content = models.TextField(blank=True, null=True)
+    price = models.DecimalField(
+        max_digits=15, decimal_places=2, default=Decimal(99.99))
