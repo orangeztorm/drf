ef23ed861266abbd50db766bb85b88f821697757
Taiwo Kenny <taiwokenny45@gmail.com>
2022-10-23 11:14:31 +0100

    refrshing api request

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,10 +1,18 @@
+import json
 from django.shortcuts import render
 from django.http import JsonResponse
 
 # Create your apis here.
 
-def api_home(request, **args,):
-    return JsonResponse({
-        'message': 'hi there first django api'
-    })
 
+def api_home(request, *args, **kwargs):
+    body = request.body
+    data = {}
+
+    try:
+        data = json.loads(body)
+    except:
+        pass
+
+    print(data.keys());
+    return JsonResponse(data)
--- py_client/basic.py
+++ py_client/basic.py
@@ -6,8 +6,8 @@ import requests
 
 endpoint = "http://localhost:8000/api/"
 
-get_response = requests.get(endpoint)  # Htt request
+get_response = requests.get(endpoint, params={'abc': 123}, json={"query": "hello"})  # Htt request
 
-print(get_response.text) # printts the data
+# print(get_response.text) # printts the data
 print(get_response.json()) # printts the data in json
-print(get_response.status_code) # printts the status code
+# print(get_response.status_code) # printts the status code
