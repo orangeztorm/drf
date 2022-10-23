fe631a35e3e93caea17209c4ed98bede03c42b19
Taiwo Kenny <taiwokenny45@gmail.com>
2022-10-23 10:42:42 +0100

    django-rest-framwork

--- py_client/basic.py
+++ py_client/basic.py
@@ -4,6 +4,8 @@ import requests
 
 endpoint = "https://httpbin.org/anything"
 
+endpoint = "http://localhost:8000/"
+
 get_response = requests.get(endpoint)  # Htt request
 
 print(get_response.text) # printts the data
