31f1fc87a3b2c1c85dc22728e3ba4e3062c3e846
Taiwo Kenny <taiwokenny45@gmail.com>
2022-10-30 12:15:18 +0100

    funsction based view

--- backend/products/urls.py
+++ backend/products/urls.py
@@ -5,6 +5,8 @@ from . import views
 # /api/products/
 
 urlpatterns = [
-    path('', views.product_list_create_view),
-    path('<int:pk>/', views.product_detail_view),
+    # path('', views.product_list_create_view),
+    path('', views.product_alt_view),
+    # path('<int:pk>/', views.product_detail_view),
+    path('<int:pk>/', views.product_alt_view),
 ]
--- backend/products/views.py
+++ backend/products/views.py
@@ -4,6 +4,14 @@ from .models import Product
 
 from .serializers import ProductSerializer
 
+from rest_framework.response import Response
+
+from rest_framework.decorators import api_view
+
+from django.shortcuts import get_object_or_404
+
+from django.http import Http404
+
 
 class ProductListCreateApiView(generics.ListCreateAPIView):
     queryset = Product.objects.all()
@@ -42,3 +50,31 @@ class ProductListAPIView(generics.ListAPIView):
 
 
 product_list_view = ProductListAPIView.as_view()
+
+
+@api_view(["GET", "POST"])
+def product_alt_view(request, pk=None, *args, **kwargs):
+    method = request.method
+
+    if method == "GET":
+        if pk is not None:
+            # get request -> details view
+            obj = get_object_or_404(Product, pk=pk)
+            data = ProductSerializer(obj, many=False).data
+            return Response(data)
+
+        # list view
+        queryset = Product.objects.all()
+        data = ProductSerializer(queryset, many=True).data
+        return Response(data)
+
+    if method == "POST":
+        # create an item
+        serializer = ProductSerializer(data=request.data)
+        if serializer.is_valid(raise_exception=True):
+            title = serializer.validated_data.get('title')  # type: ignore
+            content = serializer.validated_data.get('content') or None  # type: ignore
+            if content is None:
+                content = title
+            serializer.save(content=content)
+            return Response(serializer.data)
