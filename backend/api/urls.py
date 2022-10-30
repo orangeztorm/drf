1b113bd112cc71fc92e40ba071f557cc7494e373
Taiwo Kenny <taiwokenny45@gmail.com>
2022-10-30 10:40:19 +0100

    generic view

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.api_home) # localhost/8000/api/
+    path('', views.api_home), # localhost/8000/api/
+    # path('products/', include('products.urls'))
 ]
